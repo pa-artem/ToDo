e1e3bf3353ca516f756b8bc361534668264efa96
pa-artem <pa.artem20@yandex.ru>
2021-09-07 15:15:36 +0300

    Add task items

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,3 +1,32 @@
-"use strict";
+'use strict';
 
 import 'normalize.css';
+
+function taskHtml(text, onDelete, onChecked) {
+  const root = document.createElement('div');
+  root.classList.add('task');
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.addEventListener('click', onChecked);
+  root.appendChild(checkbox);
+
+  const taskText = document.createElement('span');
+  taskText.classList.add('task-text');
+  taskText.textContent = text;
+  root.appendChild(taskText);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('delete-button', 'btn');
+  deleteButton.addEventListener('click', onDelete);
+  deleteButton.textContent = '❌';
+  root.appendChild(deleteButton);
+
+  return root;
+}
+
+const tasksList = document.querySelector('.list');
+tasksList.appendChild(taskHtml('test'));
+tasksList.appendChild(taskHtml('test'));
+tasksList.appendChild(taskHtml('test'));
+tasksList.appendChild(taskHtml('test'));
