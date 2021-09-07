d82b348df07cdc0f9de05b5797979952d9f08aa6
pa-artem <pa.artem20@yandex.ru>
2021-09-07 16:04:49 +0300

    Implement tasks addition and removal

--- src/js/todo.js
+++ src/js/todo.js
@@ -8,7 +8,7 @@ function taskHtml(text, onDelete, onChecked) {
 
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-  checkbox.addEventListener('click', onChecked);
+  checkbox.addEventListener('click', e => onChecked(e, root));
   root.appendChild(checkbox);
 
   const taskText = document.createElement('span');
@@ -18,7 +18,7 @@ function taskHtml(text, onDelete, onChecked) {
 
   const deleteButton = document.createElement('button');
   deleteButton.classList.add('delete-button', 'btn');
-  deleteButton.addEventListener('click', onDelete);
+  deleteButton.addEventListener('click', e => onDelete(e, root));
   deleteButton.textContent = '❌';
   root.appendChild(deleteButton);
 
@@ -26,7 +26,64 @@ function taskHtml(text, onDelete, onChecked) {
 }
 
 const tasksList = document.querySelector('.list');
-tasksList.appendChild(taskHtml('test'));
-tasksList.appendChild(taskHtml('test'));
-tasksList.appendChild(taskHtml('test'));
-tasksList.appendChild(taskHtml('test'));
+const tasks = [];
+const filterSelector = document.querySelector('select.filter');
+
+const tasksPlaceholder = document.createElement('div');
+tasksPlaceholder.textContent = 'No tasks here yet...';
+tasksPlaceholder.classList.add('tasks-placeholder');
+
+function displayTasks() {
+  while (tasksList.firstChild) {
+    tasksList.removeChild(tasksList.firstChild);
+  }
+  const filtered = tasks.filter(task =>
+    filterSelector.value === 'task' ||
+    filterSelector.value === 'unsuccess' && !task.classList.contains('completed') ||
+    filterSelector.value === 'success' && task.classList.contains('completed'));
+  filtered.forEach(task => tasksList.appendChild(task));
+
+  if (filtered.length === 0) {
+    tasksList.appendChild(tasksPlaceholder);
+  }
+}
+
+displayTasks();
+
+const onTaskCheck = (e, task) => {
+  task.classList.toggle('completed');
+  displayTasks();
+};
+
+const onTaskDelete = (e, task) => {
+  const i = tasks.indexOf(task);
+  if (i >= 0) {
+    tasks.splice(i, 1);
+  }
+  displayTasks();
+};
+
+const taskTextInput = document.querySelector('input.field');
+const addTaskButton = document.querySelector('button.add');
+
+function addTask() {
+  const taskText = taskTextInput.value.trim();
+  if (taskText === '') {
+    return;
+  }
+  const task = taskHtml(taskText, onTaskDelete, onTaskCheck);
+  tasks.push(task);
+  taskTextInput.value = '';
+  displayTasks();
+}
+
+addTaskButton.addEventListener('click', addTask);
+taskTextInput.addEventListener('keyup', ({ key }) => {
+  if (key === 'Enter') {
+    addTask();
+  }
+});
+
+filterSelector.addEventListener('input', () => {
+  displayTasks();
+});
